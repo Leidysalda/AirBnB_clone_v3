695742d5032d4af63166cd2dd94f5c819e3e946c
leidysalda <leidysalda1@gmail.com>
2020-09-10 23:41:14 +0000

    [Feature]Some stats - Endpoint that retrieves the number of each objects by type api/v1/app.py api/v1/views/index.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,4 +1,4 @@
-#!/urs/bin/python3
+#!/usr/bin/python3
 """create a modiule Flask"""
 
 from flask import Flask
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -10,3 +10,15 @@ from flask import jsonify
 def status():
     """status"""
     return jsonify({"status": "OK"})
+
+@app_views.route('/')
+@app_views.route('/stats')
+def count_all():
+    """number of each objects"""
+
+    return jsonify({"amenities": storage.count('Amenity'),
+                    "cities": storage.count('City'),
+                    "places": storage.count('Place'),
+                    "reviews": storage.count('Review'),
+                    "states": storage.count('State'),
+                    "users": storage.count('User')})
