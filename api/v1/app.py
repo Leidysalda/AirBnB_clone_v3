b7d8be94106fed4154013e007d685e355fb0776b
juan-skill <1294@holbertonschool.com>
2020-09-11 01:31:19 +0000

    [feature] 6. Not found: create a handler for 404 errors modified:   api/v1/app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """create a modiule Flask"""
 
-from flask import Flask
+from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
 from os import getenv
@@ -15,6 +15,12 @@ app.register_blueprint(app_views)
 def close_session(self):
     """remove the current SQLAlchemy session"""
     storage.close()
+
+
+@app.errorhandler(404)
+def error_route(e):
+    """ create a web page 404 """
+    return jsonify({'error': 'Not found'}), 404
 
 
 if __name__ == '__main__':
