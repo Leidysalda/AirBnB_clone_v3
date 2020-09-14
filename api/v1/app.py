69755305526064c256e77ed33733d9ef56d34e02
juan-skill <1294@holbertonschool.com>
2020-09-14 19:00:30 +0000

    [feature] 13. HTTP access control (CORS): A resource makes a cross-origin HTTP request when it requests a resource from a different domain, modified:   api/v1/app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -5,10 +5,14 @@ from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
 from os import getenv
+from flask_cors import CORS
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 app.register_blueprint(app_views)
+
+
+cors = CORS(app, resources={r'/api/v1/*': {'origins': '0.0.0.0'}})
 
 
 @app.teardown_appcontext
